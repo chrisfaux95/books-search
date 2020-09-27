ed69a669ba36d462dc26fb6074b250393905a61e
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-26 22:36:16 -0400

    added mongo integration

---
+++ controllers/bookController.js
@@ -0,0 +1,50 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: (req, res) => {
+        // console.log("req.query", req.query);
+        db.Book.find(req.query).then(
+            dbBook => res.json(dbBook)
+        ).catch(
+            err => res.status(422).json(err)
+        );
+    },
+
+    findById: (req, res) => {
+        db.Book.findById(req.params.id).then(
+            dbModel => res.json(dbModel)
+        ).catch(
+            err => res.status(422).json(err)
+        );
+    },
+
+    create: (req, res) => {
+        db.Book.create(req.body).then(
+            dbModel => res.json(dbModel)
+        ).catch(
+            err => res.status(422).json(err)
+        );
+    },
+
+    update: (req, res) => {
+        db.Book.findOneAndUpdate(
+            { _id: req.params.id }, req.body
+        ).then(
+            dbModel => res.json(dbModel)
+        ).catch(
+            err => res.status(422).json(err)
+        );
+    },
+
+    remove: (req, res) => {
+        db.Book.findById(
+            { _id: req.params.id }
+        ).then(
+            dbModel => dbModel.remove()
+        ).then(
+            dbModel => res.json(dbModel)
+        ).catch(
+            err => res.status(422).json(err)
+        );
+    }
+}
